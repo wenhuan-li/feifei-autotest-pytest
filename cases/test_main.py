42cb882db610f7105ec4df12c40a97d56a914e9b
wenhuan-li <wenhuan.li@philips.com>
2024-08-03 10:26:06 +0800

    移除用例标题里的参数

--- cases/test_main.py
+++ cases/test_main.py
@@ -12,6 +12,7 @@ def test_http_api(case_data):
     instance = None
     allure.dynamic.suite(f"Case {case_data.get('case_id')}: {case_data.get('case_name')}")
     allure.dynamic.title(f"Step {case_data.get('step_id')}: {case_data.get('step_name')}")
+    allure.dynamic.parameter(case_data, "(empty)", mode="hidden")
     print(f"\nTest Case => {case_data}")
 
     route = case_data.get("route")
