c648f5972ab413d5444f8f3dd2e8f74a27a4f47a
wenhuan-li <wenhuan.li@philips.com>
2024-08-07 17:23:33 +0800

    description转为正文

--- utils/assert_util.py
+++ utils/assert_util.py
@@ -11,7 +11,7 @@ def assert_list(actual_list, expect_list, description=None, message=""):
 
 def assert_dict(actual_dict, expect_dict, description=None, message=""):
     if description and len(description) > 0:
-        allure.dynamic.description(f"##### {description}")
+        allure.dynamic.description(f"{description}")
     for key, actual in actual_dict.items():
         if key not in expect_dict:
             continue
@@ -27,7 +27,7 @@ def assert_dict(actual_dict, expect_dict, description=None, message=""):
 
 def assert_string(actual, expect, description=None, message=""):
     if description and len(description) > 0:
-        allure.dynamic.description(f"##### {description}")
+        allure.dynamic.description(f"{description}")
     _message = f"Test: {actual} | {expect}\t{message}"
     print(_message)
     with allure.step(_message):
@@ -36,7 +36,7 @@ def assert_string(actual, expect, description=None, message=""):
 
 def assert_none(expect, description=None, message=""):
     if description and len(description) > 0:
-        allure.dynamic.description(f"##### {description}")
+        allure.dynamic.description(f"{description}")
     _message = f"Data not found is right\t{message}"
     print(_message)
     with allure.step(_message):
