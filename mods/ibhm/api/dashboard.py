da96f34bae9ce329d80656ac6276cd5afdbca750
wenhuan-li <wenhuan.li@philips.com>
2024-08-07 10:00:41 +0800

    调整了dashboard接口,现在查询从用例中获取

--- mods/ibhm/api/dashboard.py
+++ mods/ibhm/api/dashboard.py
@@ -38,23 +38,19 @@ class Dashboard:
     def __init__(self):
         pass
 
-    def get_latest_data_update_time(self):
+    def get_latest_data_update_time(self, parameter):
         url = f"{base_url}/getLatestDataUpdateTime"
         actual = HttpUtil().get(url)
         actual = actual.get("content").get("latestDataUpdateTime").replace("T", " ")
-        select = "SELECT MAX(completion_time) AS max_create_at FROM t_questionnaire_instance"
-        expect = connector.connect().execute(select)
+        expect = connector.connect().execute(parameter.get("query"))
         expect = datetime.strftime(expect[0]["max_create_at"], "%Y-%m-%d %H:%M:%S")
-        assert_string(actual, expect, description=select)
+        assert_string(actual, expect, description=parameter)
         return self
 
     def dashboard_ib(self, parameter):
         url = f"{base_url}/ib"
-        area = parameter.get("area")
-        actual = HttpUtil().get(url, params={"area": conv_area_cn_en(area)}).get("content")
-        area = "'东区','西区','南区','北区'" if area == "全国" else f"'{area}'"
-        select = f"SELECT d.modality, count(*) FROM device_view d INNER JOIN ib_platinum_list_view ib ON d.serial_num = ib.equipment AND d.modality = ib.modality WHERE ib.platinum = true AND area IN ({area}) GROUP BY d.modality"
-        record = connector.connect().execute(select)
+        actual = HttpUtil().get(url, params={"area": parameter.get("area")}).get("content")
+        record = connector.connect().execute(parameter.get("query"))
         expect = {}
         total = 0
         for item in record:
@@ -63,7 +59,7 @@ class Dashboard:
             expect[key] = item["count"]
             total += item["count"]
         expect["ibTotal"] = total
-        assert_dict(actual, expect, description=select, message=f"白金保IB数据_{area}")
+        assert_dict(actual, expect, description=parameter)
         return self
 
     def modality_health_status(self, parameter):
@@ -72,22 +68,12 @@ class Dashboard:
         modality = parameter.get("modality", None)
         time_condition = parameter.get("time_condition", "month")
         params = {
+            "area": area,
             "modality": modality,
-            "area": conv_area_cn_en(area),
             "time_condition": time_condition
         }
         actual = HttpUtil().get(url, params=params).get("content")
-        area = "'东区','西区','南区','北区'" if area == "全国" else f"'{area}'"
-        select = "SELECT health_status, count(*) FROM device_view WHERE 1 = 1"
-        if area:
-            select += f" AND area in ({area})"
-        if modality:
-            select += f" AND modality = '{modality}'"
-        if time_condition:
-            time_int = conv_time_en_int(time_condition)
-            select += f" AND completion_time >= (CURRENT_TIMESTAMP  - INTERVAL '{time_int} days')"
-        select += " GROUP BY health_status"
-        expect = connector.connect().execute(select)
+        expect = connector.connect().execute(parameter.get("query"))
         expect = [{"deviceNum": item["count"], "healthStatus": item["health_status"]} for item in expect]
-        assert_list(actual, expect, description=select, message=f"当前设备健康状况-{area}")
+        assert_list(actual, expect, description=parameter)
         return self
